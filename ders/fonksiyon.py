50b82d0b8b1cba988b7c10d11abef8e991717f12
beyzaunsal <unsalbeyza678@gmail.com>
2024-07-24 20:45:43 +0300

    fonksiyon

---
+++ ders/fonksiyon.py
@@ -0,0 +1,20 @@
+"""
+def selamla():
+    print("Merhaba")
+    print("Nasılsın?")
+selamla()    
+selamla()
+
+a=2
+b=3
+def merhabade():
+    print("Merhaba"*a)    
+merhabade()    
+
+def topla():
+    print(3+2)
+topla()
+"""
+def topla(a+b):
+    print(a+b)
+topla(60,5)    
