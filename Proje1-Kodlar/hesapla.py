8e8867daede9257328af886f730d00212ff03f1a
beyzaunsal <unsalbeyza678@gmail.com>
2024-07-29 19:33:27 +0300

    güncelleme

---
+++ Proje1-Kodlar/hesapla.py
@@ -0,0 +1,12 @@
+def toplama(a,b):
+     return a+b
+
+def çıkarm (a-b):
+     return a-b 
+
+def çarpma(a*b):
+     retun a*b
+
+def bölme(a/b):
+      return a/b
+
--- Proje1-Kodlar/hesapmakinesi.py
+++ Proje1-Kodlar/hesapmakinesi.py
@@ -8,18 +8,6 @@ print("║  Seçimini yap:   ║")
 print("╚"+"══════════════════"+"╝")
 soru=int(input("Seçimi yap:")) 
 
-def toplama(a,b):
-     return a+b
-
-def çıkarm (a-b):
-     return a-b 
-
-def çarpma(a*b):
-     retun a*b
-
-def bölme(a/b):
-      return a/b
-
 
 
 
