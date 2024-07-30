b45904339ecb67751b7664a3cad58012c16fe972
beyzaunsal <unsalbeyza678@gmail.com>
2024-07-30 17:28:39 +0300

    değişim

--- Proje1-Kodlar/hesaplama/hesapmakinesi.py
+++ Proje1-Kodlar/hesaplama/hesapmakinesi.py
@@ -1,3 +1,4 @@
+konu= "Hesap Makinesi"
 print("╔"+"══════════════════"+"╗")
 print("║ Hesap Makinesi   ║")
 print("║  1- Toplama      ║")
@@ -5,25 +6,17 @@ print("║  2- Çıkarma      ║")
 print("║  3- Çarpma       ║")
 print("║  4- Bölme        ║")
 print("║  Seçimini yap:   ║")
-print("╚"+"══════════════════"+"╝")
-soru=int(input("Seçimi yap:")) 
-if soru==1 :
-     print("Toplama işlemini seçtiniz.") 
-elif soru==2 : 
-     print("Çıkarma işlemini seçtiniz.")  
-elif soru==3 : 
-     print("Çarpma işlemini seçtiniz.")
-else:
-     print("Bölme işlemini seçtiniz. ")
-     
-a= int(input("ilk sayıyı giriniz"))
-b= int(input("ikinci sayıyı giriniz"))
+print("╚"+"══════════════════"+"╝") 
+soru=int(input("Seçimi yap:"))
+        
+a= int(input("ilk sayıyı giriniz: "))
+b= int(input("ikinci sayıyı giriniz: ")) 
 
 def toplama(a,b):
      return a+b
 
 def çıkarma(a,b):
-     return a-b 
+     return a-b
 
 def çarpma(a,b):
      return a*b
@@ -31,4 +24,12 @@ def çarpma(a,b):
 def bölme(a,b):
       return a/b
 
-
+if soru==1 : 
+      print("Sonuç: ", toplama(a,b))
+elif soru==2 :
+      print("Sonuç: ", çıkarma(a,b))
+elif soru==3 :
+     print("Sonuç: ", çarpma(a,b))
+else :
+     print("Sonuç: ", bölme(a,b))
+    
--- Proje1-Kodlar/uygulama.py
+++ Proje1-Kodlar/uygulama.py
@@ -1,5 +1,9 @@
 import hesaplama.hesapmakinesi as hm
-print(hm.Toplama(8,9))
-#import hesaplama.hesapla as hm
-#print(hm.toplama(8,9)
- 
+print(hm.toplama(a,b))
+print(hm.çıkarma(a,b))
+print(hm.çarpma(a,b))
+print(hm.bölme(a,b))
+
+
+
+     
