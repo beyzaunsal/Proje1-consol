b2a938be45362211a19c45dadf0594e953c8b697
beyzaunsal <unsalbeyza678@gmail.com>
2024-07-31 19:13:53 +0300

    güncelleme

--- Proje1-Kodlar/hesaplama/hesapmakinesi.py
+++ Proje1-Kodlar/hesaplama/hesapmakinesi.py
@@ -1,35 +1,35 @@
-def hesapmakinesi():
+def hmmenü():
      print("╔"+"══════════════════"+"╗")
-print("║ Hesap Makinesi   ║")
-print("║  1- Toplama      ║")
-print("║  2- Çıkarma      ║")
-print("║  3- Çarpma       ║")
-print("║  4- Bölme        ║")
-print("║  Seçimini yap:   ║")
-print("╚"+"══════════════════"+"╝") 
-soru=int(input("Seçimi yap:"))
-        
-a= int(input("ilk sayıyı giriniz: "))
-b= int(input("ikinci sayıyı giriniz: ")) 
+     print("║ Hesap Makinesi   ║")
+     print("║  1- Toplama      ║")
+     print("║  2- Çıkarma      ║")
+     print("║  3- Çarpma       ║")
+     print("║  4- Bölme        ║")
+     print("║  Seçimini yap:   ║")
+     print("╚"+"══════════════════"+"╝") 
+     soru=int(input("Seçimi yap:"))
+          
+     a= int(input("ilk sayıyı giriniz: "))
+     b= int(input("ikinci sayıyı giriniz: ")) 
 
-def toplama(a,b):
-     return a+b
+     def toplama(a,b):
+          return a+b
 
-def çıkarma(a,b):
-     return a-b
+     def çıkarma(a,b):
+          return a-b
 
-def çarpma(a,b):
-     return a*b
+     def çarpma(a,b):
+          return a*b
 
-def bölme(a,b):
-      return a/b
+     def bölme(a,b):
+          return a/b
 
-if soru==1 : 
-      print("Sonuç: ", toplama(a,b))
-elif soru==2 :
-      print("Sonuç: ", çıkarma(a,b))
-elif soru==3 :
-     print("Sonuç: ", çarpma(a,b))
-else :
-     print("Sonuç: ", bölme(a,b))
-    
+     if soru==1 : 
+          print("Sonuç: ", toplama(a,b))
+     elif soru==2 :
+          print("Sonuç: ", çıkarma(a,b))
+     elif soru==3 :
+          print("Sonuç: ", çarpma(a,b))
+     else :
+          print("Sonuç: ", bölme(a,b))
+     
--- Proje1-Kodlar/uygulama.py
+++ Proje1-Kodlar/uygulama.py
@@ -1,5 +1,52 @@
-#import hesaplama.anamenü as amenü
-#print(amenü.anamenü())
+import hesaplama.hesapmakinesi
+def anamenü():
+    print("╔"+"═════════════════════════════════════"+"╗")
+    print("║ VEKTOREL 1. PROJE ANA MENÜ          ║")
+    print("║  1- HESAP MAKİNESİ                  ║")
+    print("║  2- MÜZİK TÜRLERİ HAKKINDA BİLGİ    ║")
+    print("║  3- NOT ORTALAMASI                  ║")
+    print("║  4- DAİRENİN ÇAP ve ALAN HESABI     ║")
+    print("║  5- BİLMECE OYUNU                   ║")
+    print("║  6- CİSMİN DÜŞME SÜRESİ             ║")
+    print("║  7- BOY KİLO ENDEKSİ                ║")
+    print("║  8- YAŞ HESABI                      ║")
+    print("║  9- FAİZ GETİRİSİ                   ║")
+    print("║  10- RİTMİK SAYMA                   ║")
+    print("║  11- ÇIKIŞ                          ║")
+    print("║            Seçimini yap:            ║")
+    print("╚"+"═════════════════════════════════════"+"╝")
+    soru=int(input("Seçimini yap:"))
+    print()
+    if soru == 1 : 
+        hesaplama.hesapmakinesi.hmmenü()
+        print("Hesap Makinesini seçtiniz.")
+    elif soru ==2 : 
+        print("Müzik türleri hakkında bilgi edinmek istediniz.")
+    elif soru ==3 :
+        print("Not ortalamasını seçtiniz.") 
+    elif soru ==4 :
+        print("Dairenin çap ve alanını seçtiniz.")
+    elif soru==5 :
+        print("Bilmece oyununu seçtiniz.")
+    elif soru==6 :
+        print("Cismin düşme süresini seçtiniz.")
+    elif soru==7 :
+        print("Boy kilo endeksini seçtiniz.")
+    elif soru==8 :
+        print("Yaş hesabı seçtiniz.")
+    elif soru==9 :
+        print("Faiz getirisini seçtiniz.")
+    elif soru==10 :
+        print ("Ritmik saymayı seçtiniz.")
+    elif soru==11 :
+        print("Çıkış yapmak istediniz.")
+    else: 
+        anamenü()
+       
+
+
+import hesaplama.anamenü as amenü
+
 
 #import hesaplama.hesapmakinesi as hm
 #print(hm.toplama(a,b))
