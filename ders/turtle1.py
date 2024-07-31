00cc26712df4fa3ffc449195635c6a802bdfec23
beyzaunsal <unsalbeyza678@gmail.com>
2024-07-31 21:11:09 +0300

    güncel

--- ders/turtle1.py
+++ ders/turtle1.py
@@ -9,7 +9,7 @@ for x in range(20,300,1):
     turtle.right(20)
 
 input()   
-'''
+
 
 #import random
 #print(random.random(1,100))
@@ -23,3 +23,35 @@ for a in range (10):
     turtle.goto(random.randint(-200,200),random.randint(-200,200))
     turtle.down()
 
+
+not5=45
+print(type(not5))
+sehir="ankara"
+print(type(sehir))
+a=4.5
+print(type(a))
+
+tt=aa=vv=8
+print(aa*2)
+
+adi, soyadi, numarasi= "beyza", "ünsal", 20370075
+print(adi, soyadi, numarasi)
+
+print(f"tt değişkeni{type(tt)}türünde ve değeri {tt}")
+print(tt*2)
+tt = str(tt)
+print(tt*2)
+'''
+a=5
+a=6
+def topla():
+    a=7
+    print("içte:",a)
+def carp():
+    global a
+    a=8
+    print("çarpta:",a)
+
+topla()   
+print("dışta:",a) 
+carp()
