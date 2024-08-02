5fe41e27aab02c455ecfe5e2070dd72e6cb5d813
beyzaunsal <unsalbeyza678@gmail.com>
2024-08-02 21:02:10 +0300

    uygulama

--- ders/turtle1.py
+++ ders/turtle1.py
@@ -56,3 +56,19 @@ topla()
 print("dışta:",a) 
 carp()
 '''
+
+hak=10 
+puan=100
+for i in range(hak):
+    tahmin=int(input("tahminin ne: "))
+    if tahmin==tutulan :
+        print("bildin")
+        break
+    elif tahmin >tutulan: 
+        puan -=10
+        print()
+        print()
+    elif tahmin < tutulan:
+        puan-=10
+        print("sayın küçük")
+        print("puanın: ",puan )
