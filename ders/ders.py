26b8c4cc5b1a004c365615e1278f7871cb00d0f9
beyzaunsal <unsalbeyza678@gmail.com>
2024-08-05 21:17:04 +0300

    değişim

---
+++ ders/ders.py
@@ -0,0 +1,27 @@
+# import math 
+#print(dir(math))
+#yariçap=float(input("dairenin yarı çapı nedir?"))
+#print("çevresi: ", 2*math.pi)
+
+# help(math.pi)
+# print(math.sqrt(9))
+# print(math.sin(1))
+
+# STRİNG
+
+
+# a=input("adın ne?")
+# print(a.upper())  #hepsini büyük yaptı
+# print(a.lower()) #hepsini küçük yaptı
+# print(a.capitalize()) #ilk harfi büyütür
+# print(len(a))  #uzunluk belirtir
+
+
+######while#####
+
+
+
+a = 1
+while a>10:
+    print(a)
+    a+=2
---
+++ ders/while1.py
@@ -0,0 +1,19 @@
+#   a = 1
+#   while a>10:
+#       print(a)
+#       a+=2
+
+
+# meyveler=[]
+# meyve = " "
+# while meyve!="":
+#   meyve =input("meyve giriniz:")
+#   if meyve != "": meyveler.append(meyve)
+# else:
+#   print("="*33)
+# print("girdiğiniz meyveler: ",*meyveler, sep="\n")  
+
+# liste=[]
+# kişi=" "
+# while meyve=input("Adınızı giriniz:")
+#    if  kişi !="": liste.
