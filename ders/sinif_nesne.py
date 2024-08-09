9790cea4fcccb1423e2ebec4f4f50269e8d545ab
beyzaunsal <unsalbeyza678@gmail.com>
2024-08-09 21:12:07 +0300

    güncelleme

--- ders/sinif_nesne.py
+++ ders/sinif_nesne.py
@@ -5,21 +5,21 @@
 # print(b*2)
 # print(type(b))
 
-class Ogrenci: # referans, model
-    adi = "Mustafa"
-    soyadi = "ALP"
-    numarasi = "547"
-    def __init__(self,xx,yy):
-        self.adi = xx
-        self.no = yy
+# class Ogrenci: # referans, model
+#     adi = "Mustafa"
+#     soyadi = "ALP"
+#     numarasi = "547"
+#     def __init__(self,xx,yy):
+#         self.adi = xx
+#         self.no = yy
 
-print("Öğrenci Bilgisi: ",Ogrenci.adi, Ogrenci.soyadi)
+# print("Öğrenci Bilgisi: ",Ogrenci.adi, Ogrenci.soyadi)
 
-ogrenci1 = Ogrenci()
-print("o1:",ogrenci1.adi)
-ogrenci1.bilgi() 
-ogrenci2 = Ogrenci()
-print("o2:",ogrenci2.adi)
+# ogrenci1 = Ogrenci()
+# print("o1:",ogrenci1.adi)
+# ogrenci1.bilgi() 
+# ogrenci2 = Ogrenci()
+# print("o2:",ogrenci2.adi)
 
 # class Ogrenci():
 #     AdSoyad = "Boş"
@@ -38,3 +38,37 @@ print("o2:",ogrenci2.adi)
 # ogrenci1.bilgi() 
 # ogrenci2 = Ogrenci()
 # ogrenci2.bilgi()
+
+class Ogrenci:          #Sınıf isimlerinin baş harfi büyük hafrle yazılır.
+    AdSoyad= "tanımsız"
+    NotOrtalaması=""
+    DisiplinCezası=0
+    # aldığıDersler=["ders listesi boş"]
+
+    def __init__(self,ad,no):    #def __init__(self,ad,no=""): yaparsan numara girme zorunluluğunu kaldırır. Ayrıca__init__ olmak zorunda
+        self.AdSoyad = ad
+        self.Numara= no
+        # self.aldığıDersler.append(ad)
+        self.aldığıDersler=[]
+    
+    def bilgi(self):
+        print("\n\nmetod ile: Adı Soyadı:",self.AdSoyad,",Numarası:",self.Numara)
+        print("Dersler:",*self.aldığıDersler,sep=",") 
+
+    def ders_ekle(self,ww):
+        self.aldığıDersler.append(ww)
+
+    def ders_listesi(self):
+        print("\n\n dersler listeleniyor")
+        print(*self.aldığıDersler,sep="\n")    
+
+print("sınıftaki adsoyad değeri:",Ogrenci.AdSoyad)
+ogrenci1=Ogrenci("ahmet bal",10) 
+ogrenci2=Ogrenci("veli gül",15)
+# ogrenci1.ders_listesi()
+ogrenci1.ders_ekle("matematik")
+ogrenci1.ders_ekle("türkçe")
+ogrenci1.ders_ekle("fizik")
+ogrenci1.ders_listesi()
+ogrenci1.bilgi()       
+ogrenci2.bilgi()
